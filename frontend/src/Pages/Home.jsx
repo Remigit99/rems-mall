3b28a2166da15d9c9f9bf0f542f6fbb802e9bfea
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-09-22 20:45:10 +0100

    create header

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,30 @@
-
+import headerImg from "../assets/header_image_o.png";
+import { BsTruck } from "react-icons/bs";
+import { MdPayment } from "react-icons/md";
+import { IoGiftOutline } from "react-icons/io5";
+import { RiCustomerService2Fill } from "react-icons/ri";
 
 const Home = () => {
   return (
-    <div>Home
-      <h1 className="text-red-900 text-3xl">Hello World!</h1>
+    <section className="bg-[#ef9e8580] ">
+      <div className="header__container pt-28 grid grid-cols-2 place-items-center gap-20  ">
+        <div>
+          <h1 className="text-white text-5xl">The Best Collections 2024</h1>
+        </div>
 
-    </div>
-  )
-}
+        <div>
+          <img src={headerImg} alt="header_img" />
+        </div>
+      </div>
 
-export default Home
+      <div>
+        <BsTruck />
+        <MdPayment />
+        <IoGiftOutline />
+        <RiCustomerService2Fill />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
