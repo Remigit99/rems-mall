dd02861be627ad6cacf330a8cfa52ff45f5ec546
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-09-22 17:52:56 +0100

    add Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,36 @@
-
+import { Link } from "react-router-dom";
+// import {Camera} from "lucide-react"
+import { FaRegUser } from "react-icons/fa6";
+import { CiHeart } from "react-icons/ci";
+import { LiaShoppingBagSolid } from "react-icons/lia";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#F46FAD]">
+    <nav className=" nav  w-full my-0 mx-auto fixed top-0 left-0 h-16 flex justify-between align-center ">
+      <div className="container w-[96%] md:w-[90%] lg:w-[80%] flex justify-between align-center">
+        <Link to="/" className="">
+          <h2 className="text-[#3f4146]">
+            Rem<span className="text-white">Mall</span>
+          </h2>
+        </Link>
 
+        <ul className="flex gap-8 text-white">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/men">Men</Link>
+          <Link to="/women">Women</Link>
+          <Link to="/eletronic">Eletronic</Link>
+          <Link to="/contact">Contact</Link>
+        </ul>
+
+        <div className="flex align-middle gap-6 text-white">
+          <FaRegUser className="text-xl" />
+          <CiHeart className="text-xl"/>
+          <LiaShoppingBagSolid className="text-xl"/>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
